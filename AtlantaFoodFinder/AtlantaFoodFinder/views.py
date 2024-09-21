e0425fe2ad52f8401872f72b797d62c0a06efc6f
Ivan Yakubovich <ivanyakubovich@lawn-128-61-74-134.lawn.gatech.edu>
2024-09-21 19:22:04 -0400

    added venv

--- AtlantaFoodFinder/AtlantaFoodFinder/views.py
+++ AtlantaFoodFinder/AtlantaFoodFinder/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.views import generic
 from .models import Location
+from django.shortcuts import render
+import requests
 
+
+from .secrets import get_api_key
+import json
 def index(request):
     print(Location.get_or_init("ChIJJ2iPNY27EmsR45MJL04zqTc"))
     return HttpResponse("Home page")
@@ -9,3 +14,45 @@ def index(request):
 class LocationView(generic.DetailView):
     model = Location
     template_name = "AtlantaFoodFinder/location.html"
+
+def search_restaurants(request):
+   cuisine = request.POST.get('cuisine', 'restaurant')  # based on search bar (type of cuisine)
+   api_key = get_api_key()
+   radius = 5000
+   api_url="https://places.googleapis.com/v1/places:searchNearby"
+
+
+   data = {
+     "includedTypes": [
+       cuisine
+     ],
+     "maxResultCount": 10,
+     "locationRestriction": {
+       "circle": {
+         "center": {
+           "latitude": 37.7937,
+           "longitude": -122.3965
+         },
+         "radius": radius
+       }
+     }
+   }
+   header = {
+   "Content-Type": 'application/json',
+   "X-Goog-Api-Key": api_key,
+   "X-Goog-FieldMask": "places.displayName"
+   }
+
+
+   response = requests.post(api_url, headers=header, data=json.dumps(data))
+
+
+   if response.status_code == 200:
+       results = response.json()
+   else:
+       results = {"error": "Unable to fetch data"}
+
+
+
+
+   return render(request, 'results.html', {'results': results})

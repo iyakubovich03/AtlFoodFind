cf8e2674ff5b65f549bb6f694b8f84a71ee19a78
emerritt8 <emerritt8@gatech.edu>
2024-09-26 23:19:15 -0400

    More precise location tracking. Makes latitude and longitude unnecessary, so may remove later

--- AtlantaFoodFinder/AtlantaFoodFinder/views.py
+++ AtlantaFoodFinder/AtlantaFoodFinder/views.py
@@ -18,11 +18,10 @@ def index(request):
     return HttpResponse("Home page")
 
 def location_detail(request, pk):
-    apikey = get_api_key()
-    address = "https://maps.googleapis.com/maps/api/js?key=" + apikey + "&libraries=maps,marker&v=beta"
+    api_key = get_api_key()
 
     return render(request, 'AtlantaFoodFinder/location.html',
-                  {'location': Location.get_or_init(pk), 'isFavorite': is_favorite(request, pk),"address": address})
+                  {'location': Location.get_or_init(pk), 'isFavorite': is_favorite(request, pk), "api_key": api_key})
 
 def is_favorite(request, pk):
     if hasattr(request.user, "account"):
